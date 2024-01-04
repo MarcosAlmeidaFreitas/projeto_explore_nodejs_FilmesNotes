4e97fb83bfc694777d2598879621247a53277900
Marcos Antonio Almeida Freitas <67704150+MarcosAlmeidaFreitas@users.noreply.github.com>
2024-01-03 21:27:33 -0300

    UserUpdate quase terminado

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -2,36 +2,74 @@
 const knex = require("../database/knex/index");
 const { hash, compare } = require("bcrypt")
 const AppError = require("../utils/AppError");
-
+const salt = 8;
 
 class UsersController{
   async create(request, response){
     const {name, email, password} = request.body;
-
-    // Checando se o usuário já está cadastrado com email
-    const checkUserExists = await knex("users").where({email : email}).first();
     
-    console.log(checkUserExists);
-
-    if(checkUserExists){
-       throw new AppError("Este email já está em uso");
-    }
-
     //Verificando se os campos estão vazios
     if(!name){
-       throw new AppError("O campo nome deve ser preenchido");
+      throw new AppError("O campo nome deve ser preenchido");
     } 
-    
+
+    if(!email){
+      throw new AppError("O campo email deve ser preenchido");
+    }
+ 
     if(!password){
       throw new AppError("O campo senha deve ser preenchido");
     }
 
+    // Checando se o usuário já está cadastrado com email
+    const checkUserExists = await knex("users").where({email : email}).first();
+    
+    if(checkUserExists){
+      throw new AppError("Este email já está em uso");
+    }
+  
+
     //criptografando a senha do usuário
-    const hashedPassword = await hash(password, 8);
+    const hashedPassword = await hash(password, salt);
 
     await knex('users').insert({name, email, password:hashedPassword});
 
     response.status(201).json(`O usuário ${name} foi criado com sucesso.`);
+  }
+
+  async update(request, response){
+    const { name, email, password, oldPassword } = request.body;
+    const id  = request.params;
+    
+    const user = await knex('users').where(id).first();
+    
+    //Verificando se o usuário existe
+    if(!user) throw new AppError("O usuário não foi encontrado");
+
+    console.log(email);
+    
+    //Verificando se existe um usuário com o email informado
+    const userWithUpdatedEmail = await knex('users').where({email: email}).first();
+
+    if(userWithUpdatedEmail && userWithUpdatedEmail.id != user.id){
+      throw new AppError("Já existe um usuário com o email informado");
+    }
+
+    user.name = name ?? user.name;
+    user.email = email ?? user.email;
+
+    if(password && !oldPassword) throw new AppError("Você precisa informar a senha antiga para definir a nova");
+
+    if(password && oldPassword){
+      const checkPassword = await compare(oldPassword, user.password);
+
+      if(!checkPassword) throw new AppError("A senha antiga não confere");
+
+      user.password = await hash(password, salt);
+    }
+    
+    await knex('users').where({id : user.id}).update(user);
+    response.status(200).json({});
   }
 }
 
