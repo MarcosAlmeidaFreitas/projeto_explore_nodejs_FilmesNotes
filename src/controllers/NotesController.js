8e65fa712f3e648101170010331e172c9306435c
Marcos Antonio Almeida Freitas <67704150+MarcosAlmeidaFreitas@users.noreply.github.com>
2024-01-05 20:13:43 -0300

    Note: create, delete e show

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -7,18 +7,54 @@ class NotesController{
     const {title, description, rating, tags} = request.body;
     const user_id = request.params.user_id;
 
-    if(!title) throw new AppError("O nome da nota é necessário");
+    if(!title) throw new AppError("O titulo da nota é necessário");
     if(!{tags}) throw new AppError("Pelo menos uma tag é necessária");
     
-    const note = await knex('notes')
+    const [ note_id ] = await knex('notes')
       .insert({ title, description, rating, user_id });
 
-    const tagsInsert = tags.map(tag => {
+    console.log(note_id);
+    
+    const tagsInsert = tags.map(name => {
       return {
-
+        note_id,
+        user_id,
+        name
       }
-    })
+    });
+
+    await knex("tags").insert(tagsInsert);
+
+    response.status(200).json();
+  }
+
+  async delete(request, response){
+    const id   = request.params.note_id;
+
+    await knex('notes').where({id : id}).delete();
+
+    response.status(200).json();
+  }
+
+  async show(request, response){
+    const id = request.params.note_id;
+
+    const note = await knex('notes').where({id : id}).first();
+    const tags = await knex('tags').where({note_id: id});
+
+    const nameTags = tags.map(tag => {
+      return tag.name;
+    });
+
+    response.status(200).json({
+      note, nameTags
+    });
+  }
+
+  async index(request, response){
+    
   }
 }
+
 
 module.exports = NotesController;
--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -4,6 +4,8 @@ const {Router} = require("express");
 const notesController = new NotesController();
 const routes =  Router();
 
-routes.use("/:user_id", notesController.create);
+routes.post("/:user_id", notesController.create);
+routes.delete("/:note_id", notesController.delete);
+routes.get("/:note_id", notesController.show);
 
 module.exports = routes;  
